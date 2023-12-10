1cd375ab00ed5a9d417880213752d642559fc9ce
Luka Radisavljevic <lukaradisavljevic30@gmail.com>
2023-12-10 04:58:51 +0100

    feat: init script prints the jwt tokens

--- init.sh
+++ init.sh
@@ -1,42 +1,33 @@
 #!/bin/bash
 
-# Check if the required arguments are provided
-if [ "$#" -ne 0 ]; then
-    echo "Usage: $0"
-    exit 1
-fi
+# Colors
+Color_Off='\033[0m' # Text Reset
+Green='\033[0;32m'  # Green
 
-# Load environment variables from the .env file
+# Check if .env file exists
 if [ -f .env ]; then
+    # Export the variables from .env file
     export $(grep -v '^#' .env | xargs)
 else
-    echo "Error: .env file not found."
+    echo -e "${Green}Error: .env file not found.${Color_Off}"
     exit 1
 fi
 
 # Check if required environment variables are set
 if [ -z "$API_ENDPOINT" ] || [ -z "$USERNAME" ] || [ -z "$PASSWORD" ]; then
-    echo "Error: API_ENDPOINT, USERNAME, and PASSWORD must be set in the .env file."
+    echo -e "${Green}Error: API_ENDPOINT, USERNAME, and PASSWORD must be set in the .env file.${Color_Off}"
     exit 1
 fi
 
-# Function to display colored text
-color_echo() {
-    local color="$1"
-    local text="$2"
-    echo -e "\e[${color}m${text}\e[0m"
-}
-
-# Loop to send POST requests with different usernames
-for i in {1..4}; do
-    CURRENT_USERNAME="${USERNAME}${i}"
-    PAYLOAD="{\"username\":\"$CURRENT_USERNAME\",\"password\":\"$PASSWORD\"}"
+# Loop to send 4 POST requests
+for i in {1..4}
+do
+    # Prepare the payload
+    PAYLOAD="{\"username\":\"${USERNAME}${i}\",\"password\":\"$PASSWORD\"}"
 
     # Make the POST request using curl
-    JWT_TOKEN=$(curl -s -X POST -H "Content-Type: application/json" -d "$PAYLOAD" "$API_ENDPOINT" | jq -r .jwtToken)
+    RESPONSE=$(curl -s -X POST -H "Content-Type: application/json" -d "$PAYLOAD" "$API_ENDPOINT")
 
-    # Display JWT token with colored output
-    color_echo "32" "API bot${i} Jason Web Token:"
-    color_echo "32" "$JWT_TOKEN"
-    echo    # Add a new line for better readability
+    # Print the response
+    echo -e "${Green}$RESPONSE${Color_Off}"
 done
